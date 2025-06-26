d79dc2aab80bb263529bb9bac22d0866e85b4d42
CesarVazquezTellez <cesarvazquezt00@gmail.com>
2025-06-26 10:43:24 -0600

    Adding Clase4.java

---
+++ paquete1/Clase4.java
@@ -0,0 +1,21 @@
+package paquete1;
+
+public class Clase4 {
+    private boolean X;
+
+    public boolean isX() {
+        return X;
+    }
+
+    public void setX(boolean x) {
+        X = x;
+    }
+
+    @Override
+    public String toString() {
+        return "Clase4 [X=" + X + "]";
+    }
+
+    
+    
+}
